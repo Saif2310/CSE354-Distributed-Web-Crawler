fb3d0008c08860861f77db8696303331cf978fad
Saif2310 <128076443+Saif2310@users.noreply.github.com>
2025-04-29 23:53:47 +0300

    Update install_dependencies.sh
    pinned a certain edition of scrapy's dependency "attrs"

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,5 +8,6 @@ pip3 install --upgrade pip
 pip3 install google-cloud-pubsub==2.13.0
 pip3 install google-cloud-storage==2.5.0
 pip3 install scrapy==2.6.2
+pip3 install attrs==22.2.0
 pip3 install elasticsearch==7.17.0
 pip3 install google-cloud-monitoring==2.12.0
